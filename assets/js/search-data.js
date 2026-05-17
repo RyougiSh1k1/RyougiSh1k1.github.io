c3db04aa6aeeff2c0eb8d13b2680b92f420c4267
RyougiSh1k1 <49345080+RyougiSh1k1@users.noreply.github.com>
2026-05-17 23:04:38 +0000

    Deploying to gh-pages from @ RyougiSh1k1/RyougiSh1k1.github.io@f8e883835b6a3c1bd542c5009fdb08103ae62675 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -18,10 +18,10 @@ ninja.data = [{
           },
         },{id: "nav-research",
           title: "Research",
-          description: "",
+          description: "Upcoming",
           section: "Navigation",
           handler: () => {
-            window.location.href = "/projects/";
+            window.location.href = "/research/";
           },
         },{id: "nav-services",
           title: "Services",
@@ -430,51 +430,51 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
+          section: "News",},{id: "research-project-1",
           title: 'project 1',
           description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
+          section: "Research",handler: () => {
+              window.location.href = "/research/1_research/";
+            },},{id: "research-project-2",
           title: 'project 2',
           description: "a project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
+          section: "Research",handler: () => {
+              window.location.href = "/research/2_research/";
+            },},{id: "research-project-3-with-very-long-name",
           title: 'project 3 with very long name',
           description: "a project that redirects to another website",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
+          section: "Research",handler: () => {
+              window.location.href = "/research/3_research/";
+            },},{id: "research-project-4",
           title: 'project 4',
           description: "another without an image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
+          section: "Research",handler: () => {
+              window.location.href = "/research/4_research/";
+            },},{id: "research-project-5",
           title: 'project 5',
           description: "a project with a background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
+          section: "Research",handler: () => {
+              window.location.href = "/research/5_research/";
+            },},{id: "research-project-6",
           title: 'project 6',
           description: "a project with no image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
+          section: "Research",handler: () => {
+              window.location.href = "/research/6_research/";
+            },},{id: "research-project-7",
           title: 'project 7',
           description: "with background image",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
+          section: "Research",handler: () => {
+              window.location.href = "/research/7_research/";
+            },},{id: "research-project-8",
           title: 'project 8',
           description: "an other project with a background image and giscus comments",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/8_project/";
-            },},{id: "projects-project-9",
+          section: "Research",handler: () => {
+              window.location.href = "/research/8_research/";
+            },},{id: "research-project-9",
           title: 'project 9',
           description: "another project with an image 🎉",
-          section: "Projects",handler: () => {
-              window.location.href = "/projects/9_project/";
+          section: "Research",handler: () => {
+              window.location.href = "/research/9_research/";
             },},{id: "teachings-data-science-fundamentals",
           title: 'Data Science Fundamentals',
           description: "This course covers the foundational aspects of data science, including data collection, cleaning, analysis, and visualization. Students will learn practical skills for working with real-world datasets.",
